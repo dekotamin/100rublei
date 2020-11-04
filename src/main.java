2ac56113f46738b6a4d92bbe69336c0a8a9a0d42
Alina Faiskhanova <nyrete@gmail.com>
2020-11-04 22:27:21 +0500

    Код приложения

---
+++ src/main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        float amount = 6455;
+        float bonus = 20;
+        int totalBonus = (int) (amount / bonus);
+        System.out.println(totalBonus);
+    }
+}
