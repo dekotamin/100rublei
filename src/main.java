5049286e49a16266ec8e85a1543cab8c4f6d9089
Alina Faiskhanova <nyrete@gmail.com>
2020-11-05 11:40:19 +0500

    Удаление из индекса src/main.java

--- src/main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        boolean registered = true;
-        float amount = 6455;
-        float bonus = 20;
-        int totalBonus = (int) (amount / bonus);
-        System.out.println(totalBonus);
-    }
-}
