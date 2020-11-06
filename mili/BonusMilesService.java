1324ea15e87bc901dc37d92d08a9237182ffe998
Alina <72618120+dekotamin@users.noreply.github.com>
2020-11-06 20:02:38 +0500

    Add files via upload
    Добавление BonusMIlesService

---
+++ mili/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = 20;
+        int miles = price / bonus;
+        if (price >= bonus) {
+        } else {
+            miles = 0;
+        }
+        return miles;
+    }
+}
